79a795e2cf84f7723b78a33bfd4f55debccbea83
Ashish Dutt <ashish.dutt8@gmail.com>
2019-04-18 12:55:30 +0800

    initial commit

---
+++ scripts/r/forest_explainer.R
@@ -0,0 +1,5 @@
+# Objective: write a function that takes as input a radom forest model and outs its explanation
+# script create date: 18/4/2019
+# script last modified on: 
+# script name: forest_explainer.R
+
